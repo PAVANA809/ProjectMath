48ff318ae837aa73efa89ee01b712a7c8cd84884
PAVANA809 <pavanhapavan@gmail.com>
2023-03-23 13:48:24 +0530

    nav bar

--- src/BodyLeft.js
+++ src/BodyLeft.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "./bodyRight/Bodyright.js";
 import "./App.js";
-
+// import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import "./App.css"
 
 
 export default function Lefttag(props) {
@@ -29,7 +30,7 @@ export default function Lefttag(props) {
                             setstate={[setValues, setSelectedItemOnClick]} />
                     )
                 })
-          }
+      }
     </div>
   );
 }
@@ -49,31 +50,32 @@ function GetUnit(props) {
     props.setstate[1](subUnit);
   };
   return (
-    <div className="unit">
-      <p onClick={() => clicked()}>
-        <span className="sign">{sign}</span>
-        {props.k}: {props.unit}{" "}
-      </p>
-      <ul className={`sub-units ${display ? "show" : "hide"}`}>
-        {props.subUnit.map((item, index) => {
-          return (
-            <li
-              key={index}
-              className={props.selectedItem === item ? "selected" : ""}
-              onClick={() => updateRight(props.k, props.unit, item)}
-            >
-              <span
-                className={`eiphen ${
-                  props.selectedItem === item ? "" : "hide"
-                }`}
+      <div className="unit">
+        <p onClick={() => clicked()}>
+          <span className="sign">{sign}</span>
+          {props.k}: {props.unit}{" "}
+        </p>
+        <ul className={`sub-units ${display ? "show" : "hide"}`}>
+          {props.subUnit.map((item, index) => {
+            return (
+              <li
+                key={index}
+                className={props.selectedItem === item ? "selected" : ""}
+                onClick={() => updateRight(props.k, props.unit, item)}
               >
-                ~
-              </span>
-              {item[0]}:{} {item[1]}
-            </li>
-          );
-        })}
-      </ul>
+                <span
+                  className={`eiphen ${
+                    props.selectedItem === item ? "" : "hide"
+                  }`}
+                >
+                  ~
+                </span>
+                {item[0]}:{} {item[1]}
+              </li>
+            );
+          })}
+        </ul>
     </div>
+    
   );
 }
--- src/homePage/Navbar.js
+++ src/homePage/Navbar.js
@@ -1,12 +1,32 @@
 import React from 'react'
-import "./home.css"
+import { useRef } from "react";
+import './navbar.css'
+import ListIcon from "@mui/icons-material/List";
+import CloseIcon from "@mui/icons-material/Close";
+
 function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
   return (
-    <div className="header">
-      <p>
-        <a href='/'>EasyMath</a>
-      </p>
-    </div>
+    <header>
+      <h3>
+        <a href="/">EasyMath</a>
+      </h3>
+      <nav ref={navRef}>
+        <a href="/">Home</a>
+        <a href="/">Login</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <CloseIcon/>
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <ListIcon />
+      </button>
+    </header>
   );
 }
 
