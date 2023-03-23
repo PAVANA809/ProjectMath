7efec20ac30432b0f795a213124bf801c31970c8
PAVANA809 <pavanhapavan@gmail.com>
2023-03-23 14:17:40 +0530

    working fine

--- src/homePage/Navbar.js
+++ src/homePage/Navbar.js
@@ -1,33 +1,13 @@
-import React from 'react'
-import { useRef } from "react";
-import './navbar.css'
-import ListIcon from "@mui/icons-material/List";
-import CloseIcon from "@mui/icons-material/Close";
-
+import React from "react";
+import "./home.css";
 function Navbar() {
-  const navRef = useRef();
-
-  const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav");
-  };
-
   return (
-    <header>
-      <h3>
+    <div className="header">
+      <p>
         <a href="/">EasyMath</a>
-      </h3>
-      <nav ref={navRef}>
-        <a href="/">Home</a>
-        <a href="/">Login</a>
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <CloseIcon/>
-        </button>
-      </nav>
-      <button className="nav-btn" onClick={showNavbar}>
-        <ListIcon />
-      </button>
-    </header>
+      </p>
+    </div>
   );
 }
 
-export default Navbar
+export default Navbar;
