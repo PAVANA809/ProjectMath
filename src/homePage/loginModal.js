17fc7daf64217a41bd43e19e622ff0d1ebd3ac68
Pavana H A <pavanhapavan@gmail.com>
2023-04-08 09:36:08 +0000

    signup

--- src/homePage/loginModal.js
+++ src/homePage/loginModal.js
@@ -2,6 +2,34 @@ import React, { useState } from "react";
 import "./loginModal.css";
 
 function LoginModal(props) {
+  const [isLogin, setisLogin] = useState(true);
+  
+  const handleClose = () => {
+    props.onClose();
+  };
+  const SignUpFun = ()=>{
+    setisLogin(false);
+  }
+  const SignIn = ()=>{
+    setisLogin(true);
+  }
+  return (
+    <div className="modal login-modal">
+      <div className="modal-content login-modal-content">
+        <span className="close" onClick={handleClose}>
+          &times;
+        </span>
+        <div>
+          <button onClick={SignIn}>SignIn</button>
+          <button onClick={SignUpFun}>SignUp</button>
+        </div>
+        {isLogin?<Login/>:<Signup/>}
+      </div>
+    </div>
+  );
+}
+
+function Login(){
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -17,19 +45,8 @@ function LoginModal(props) {
     event.preventDefault();
     // TODO: Handle login logic here
   };
-
-  const handleClose = () => {
-    props.onClose();
-  };
-
-  return (
-    <div className="modal login-modal">
-      <div className="modal-content login-modal-content">
-        <span className="close" onClick={handleClose}>
-          &times;
-        </span>
-        <p>Login</p>
-        <form onSubmit={handleSubmit}>
+  return(
+    <form onSubmit={handleSubmit}>
           <input
             type="email"
             value={email}
@@ -44,9 +61,54 @@ function LoginModal(props) {
           />
           <button type="submit">Login</button>
         </form>
-      </div>
-    </div>
-  );
+  )
+}
+
+function Signup(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: Handle login logic here
+    console.log("hello")
+  };
+  return(
+    <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={handleEmailChange}
+            placeholder="Username"
+          />
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Password"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Confirm Password"
+          />
+          <button type="submit">Sign Up</button>
+        </form>
+  )
 }
 
 export default LoginModal;
